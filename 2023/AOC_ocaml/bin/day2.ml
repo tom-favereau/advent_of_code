eb651f456bc6f905083f59bd2dd23af595abe5f7
[afaureve] <[tom.favereautpe@gmail.com]>
2023-12-11 23:17:42 +0100

    day2 caml part2

--- 2023/AOC_ocaml/bin/day2.ml
+++ 2023/AOC_ocaml/bin/day2.ml
@@ -29,26 +29,45 @@ let rec doPart1 l = match l with
 
 ;;
 
-let part1 file_name =
+let rec doPart2 l = match l with 
+        | [] -> 0, 0, 0
+        | h::t ->
+            let rec aux ll r g b = match ll with  
+                | [] -> r, g, b
+                | a::q -> let num = match split a " " with | n::_ -> int_of_string n | _ -> 0 in
+                match last a with 
+                        | Some 'e' -> aux q r g (b+num) 
+                        | Some 'd' -> aux q (r+num) g b
+                        | Some 'n' -> aux q r (g+num) b
+                        | _ -> 0, 0, 0
+            in 
+            let r1, g1, b1 = (aux (split h ",") 0 0 0) in 
+            let r2, g2, b2 = (doPart2 t) in max r1 r2, max g1 g2, max b1 b2
+
+
+;;
+
+
+let pars file_name =
   try
     let channel = open_in file_name in
-    let rec read_lines res index  =
+    let rec read_lines res1 res2 index  =
       try
         let line = input_line channel in
         let liste = match split line ":" with | _::head::_ -> split head ";" | _ -> [] in
-        if doPart1 liste then read_lines (res+index) (index+1) else read_lines res (index+1)
+        let p1 = doPart1 liste in 
+        let p2 = match doPart2 liste with r, g, b -> r*b*g in
+        if p1 then read_lines (res1+index) (res2+p2) (index+1) else read_lines res1 (res2+p2) (index+1)
       with
-      | End_of_file -> (close_in channel; res)
+      | End_of_file -> (close_in channel; res1, res2)
     in
-    read_lines 0 1
+    read_lines 0 0 1
   with
-  | Sys_error err ->( print_endline ("Error: " ^ err); 0)
+  | Sys_error err ->( print_endline ("Error: " ^ err); 0, 0)
 ;;
 
 
                       
 
-print_int (part1 "inputs/input_test02.txt");;
-print_endline "";;
-print_int (part1 "inputs/input02.txt");;
-print_endline "";;
+let a, b = (pars "inputs/input_test02.txt") in (print_int a; print_string " "; print_int b;  print_endline "");;
+let a, b = (pars "inputs/input02.txt") in (print_int a; print_string " "; print_int b; print_endline "");;
