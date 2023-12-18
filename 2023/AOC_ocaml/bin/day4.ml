b76c9acec0023f9e859b65764290ef92333fe2da
[afaureve] <[tom.favereautpe@gmail.com]>
2023-12-18 17:21:57 +0100

    day4 ocaml p2

--- 2023/AOC_ocaml/bin/day4.ml
+++ 2023/AOC_ocaml/bin/day4.ml
@@ -2,15 +2,22 @@ let split s exp = Str.split (Str.regexp exp) s;;
 
 let rec pow x n = if n = 0 then 1 else if n mod 2 = 0 then let res = (pow x (n/2)) in res*res else let res = pow x (n/2) in x*res*res;;
 
-let p1 l1 l2 =  
+let number l1 l2 =  
         let rec aux a = function 
                 | [] -> 0
                 | h::t -> if (h = a) && h <> "" && h <> " " then 1 else aux a t
-        in let rec aux2 = function | [] -> -1 | h::t -> (aux h l2) + (aux2 t) in
-        let nb = aux2 l1 in ( if nb = -1 then 0 else pow 2 nb)
+        in let rec aux2 = function | [] -> 0 | h::t -> (aux h l2) + (aux2 t) in
+        aux2 l1
 ;;
 
+let p1 l1 l2 = let nb = number l1 l2 in if nb = 0 then 0 else pow 2 (nb-1);;
 
+let p2 l = let n = List.length l in let arr = Array.make n 1 in let rec aux tmp i = function
+        | [] -> tmp
+        | [_] -> tmp+arr.(i)
+        | h::t -> for k = i+1 to min (i+h) (n-1) do arr.(k) <- arr.(k)+1*arr.(i) done; aux (tmp+arr.(i)) (i+1) t
+        in aux 0 0 l
+;; 
 
 let pars file_name =
   try
@@ -20,11 +27,12 @@ let pars file_name =
         let line = input_line channel in
         let arr = (split (List.hd (List.tl (split line ":"))) "|") in
         let r1 = p1 (split (List.hd arr) " ") (split (List.hd (List.tl arr)) " ") in 
-        read_lines (res1+r1) res2
+        let r2 = number (split (List.hd arr) " ") (split (List.hd (List.tl arr)) " ") in        
+        read_lines (res1+r1) (r2::res2)
       with
-      | End_of_file -> (close_in channel; res1, res2)
+      | End_of_file -> (close_in channel; res1, p2 (List.rev res2))
     in
-    read_lines 0 0
+    read_lines 0 []
 
   with
   | Sys_error err ->( print_endline ("Error: " ^ err); 0, 0)
