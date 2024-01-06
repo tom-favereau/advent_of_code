c7e26362cb6467a71942aa9215a754ed003f0adf
[afaureve] <[tom.favereautpe@gmail.com]>
2024-01-06 15:10:33 +0100

    day16 ocaml p1 + p2

--- 2023/AOC_ocaml/bin/day16.ml
+++ 2023/AOC_ocaml/bin/day16.ml
@@ -1,45 +1,68 @@
-let split s exp = Str.split (Str.regexp exp) s;;
+open AOC_ocaml_lib.Utils 
 
-
-
-let pars file_name =
-  try
-    let channel = open_in file_name in
-    let rec read_lines tmp  =
-      try
-        let line = input_line channel in
-        let arr = Array.make (String.lenght line) '' in
-        for i = 0 to String.lenght line-1 do
-            arr.(i) <- line.[i]
-        done;
-        read_lines arr::tmp
-      with
-      | End_of_file -> (close_in channel; tmp)
+module Day16 : sig 
+  val part1 : string -> int
+  val part2 : string -> int
+end = struct 
+  type dir = Up | Down | Right | Left
+  
+  let dfs ps ds matrix = 
+    let n = Array.length matrix in
+    let visited = Hashtbl.create (n*n) in
+    let count = Hashtbl.create (n*n) in
+    let rec aux (i, j) d = 
+      if not ((Hashtbl.mem visited (i, j, d)) || (i < 0) || (i >= n) || (j < 0) || (j >= n)) then 
+      begin   
+      if not (Hashtbl.mem count (i, j)) then Hashtbl.add count (i, j) 0;  
+        Hashtbl.add visited (i, j, d) 0;
+        match matrix.(i).(j), d with 
+        | ("." | "|"), Up -> aux (i-1, j) Up 
+        | ("." | "|"), Down -> aux (i+1, j) Down
+        | ("." | "-"), Right -> aux (i, j+1) Right
+        | ("." | "-"), Left -> aux (i, j-1) Left
+        | "|", (Right | Left) -> (aux (i-1, j) Up; aux (i+1, j) Down)
+        | "-", (Up | Down) -> (aux (i, j+1) Right; aux (i, j-1) Left)
+        | ("/", Up | "\\", Down) -> aux (i, j+1) Right
+        | "/", Down | "\\", Up -> aux (i, j-1) Left
+        | "/", Left | "\\", Right -> aux (i+1, j) Down
+        | "/", Right | "\\", Left -> aux (i-1, j) Up
+        | _ -> failwith "patern matching"
+    end
+    in (aux ps ds; 
+    Hashtbl.length count)   
+    
+  
+  let part1 file_name = 
+    let matrix = match Pars.read_to_matrix file_name with 
+      | None -> failwith "parsing"
+      | Some matrix -> matrix
     in
-    let tmp = read_lines []
-    let arr = Array.make (List.lenght tmp) (Array.make (Array.length (List.head tmp)) '') in
-    let toArr c = function | [] -> () | h::t -> (arr.(c) <- h; toArr (tmp+1) t) in
-    toArr tmp
-  with
-  | Sys_error err ->( print_endline ("Error: " ^ err); 0, 0)
-;;
+    dfs (0, 0) Right matrix 
 
-type info = {i : int; j : int; dir : string};;
+  let part2 file_name = 
+    let matrix = match Pars.read_to_matrix file_name with 
+      | None -> failwith "parsing"
+      | Some matrix -> matrix 
+    in 
+    let n = Array.length matrix in
+    [
+    List.init n (fun i -> dfs (0, i) Down matrix) |> List.fold_left max 0;
+    List.init n (fun i -> dfs (n-1, i) Up matrix) |> List.fold_left max 0;
+    List.init n (fun i -> dfs (i, 0) Right matrix) |> List.fold_left max 0;
+    List.init n (fun i -> dfs (i, n-1) Left matrix) |> List.fold_left max 0;
+    ] |> List.fold_left max 0
 
-let propagation arr =
-    let ener = Array.make n (Array.make m false) in
-    let visited = Hashtbl.create 10 in
-    let rec dfs i j dir =
-        let k = {i:i; j:j; dir;dir} in
-        if Hashtbl.mem visited k then ()
-        else 
-
-let p1 file_name =
-    let arr = pars file_name in
-    let n = Array.length arr in
-    let m = Array.length arr.(0) in
-    let dfs i j dir =
+end
 
 
-let a, b = (pars "inputs/input_test02.txt") in (print_int a; print_string " "; print_int b;  print_endline "");;
-let a, b = (pars "inputs/input02.txt") in (print_int a; print_string " "; print_int b; print_endline "");;
+let () =
+  if Array.length Sys.argv <> 2 then begin
+    Printf.printf "Usage: %s <filename>\n" Sys.argv.(0);
+    exit 1;
+  end else 
+    let file_name = Sys.argv.(1) in
+    let r1 = Day16.part1 file_name in 
+    let r2 = Day16.part2 file_name in 
+    (print_int r1; print_newline (); print_int r2; print_newline ())
+  
+
