6a22a1c675584d0adb0b2aa69c82b9a1b6b254b5
[afaureve] <[tom.favereautpe@gmail.com]>
2024-12-01 21:05:46 +0100

    day 1 4 lines python

---
+++ 2024/day1/1.py
@@ -0,0 +1,4 @@
+import sys
+with open(sys.argv[1], "r") as file:
+    l1, l2 = map(sorted, zip(*[(list(map(int, line.split()))) for line in file if line.strip()]))
+    print(f"{sum(abs(x - y) for x, y in zip(l1, l2))}\n{sum(x * l2.count(x) for x in l1)}")
